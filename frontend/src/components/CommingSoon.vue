5e45fb37100871cc6a04f7f80748cf9cf0249b7d
Amar Karthick P <karthickpamar@gmail.com>
2026-05-23 11:30:35 +0530

    feat: add Comming Soon component

---
+++ frontend/src/components/CommingSoon.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="fixed inset-0 flex items-center bg-bakcground justify-center px-4">
+        <div class="relative flex flex-col items-center">
+
+            <!-- Main Text -->
+            <h1
+                class="text-[2.5rem] font-medium relative z-10
+                bg-gradient-to-t from-background via-primary to-primary
+                bg-clip-text text-transparent"
+            >
+                COMING SOON!
+            </h1>
+
+            <!-- Mirror Reflection -->
+            <h1
+                class="text-[2.5rem] font-medium scale-y-[-1]
+                bg-gradient-to-b from-primary/60 to-transparent
+                bg-clip-text text-transparent
+                opacity-40 blur-[1px] -mt-5"
+            >
+                COMING SOON!
+            </h1>
+
+        </div>
+    </div>
+</template> 
